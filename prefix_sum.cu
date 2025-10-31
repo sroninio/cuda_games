5a3eaea31c60548398cc71749faf2128bcd9602a
RS <rspiegelman@nvidia.com>
2025-10-31 12:46:24 +0200

    compilation error
    Signed-off-by: RS <rspiegelman@nvidia.com>

--- prefix_sum.cu
+++ prefix_sum.cu
@@ -5,10 +5,10 @@
 __global__ void unite_step_kernel(float *d_array, int n, int solved_block_size) {
     int t_idx = blockIdx.x * blockDim.x + threadIdx.x;
     int base_indx = (t_idx / solved_block_size) * solved_block_size;
-    int pusher_indx = base_indx + solved_block_size - 1
-    int pushed_indx = base_indx + solved_block_size + t_indx % solved_block_size;
+    int pusher_indx = base_indx + solved_block_size - 1;
+    int pushed_indx = base_indx + solved_block_size + t_idx % solved_block_size;
     if (pushed_indx < n) {
-        d_array[pushed_indx] += d_array[pusher_indx]
+        d_array[pushed_indx] += d_array[pusher_indx];
     }
 }
 
@@ -25,32 +25,27 @@ int main() {
     cudaMalloc(&d_array, bytes);
     cudaMemcpy(d_array, h_array, bytes, cudaMemcpyHostToDevice);
     
-    printf("Launching kernels with %d blocks and %d threads per block\n\n", 
-           blocksPerGrid, threadsPerBlock);
-    for (int solved_block_size = 1, solved_block_size < N; solved_block_size *= 2) {
+    printf("Launching kernels");
+    for (int solved_block_size = 1; solved_block_size < N; solved_block_size *= 2) {
         int threadsPerBlock = 256;
         int blocksPerGrid = ((N/2 + 1) + threadsPerBlock - 1) / threadsPerBlock;
         unite_step_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_array, N, solved_block_size);
         cudaDeviceSynchronize();
     }
-
-    printf("Finished kernels with %d blocks and %d threads per block\n\n", 
-           blocksPerGrid, threadsPerBlock);
-
+    printf("Finishing kernels");
+    cudaMemcpy(h_array, d_array, bytes, cudaMemcpyDeviceToHost);
     int sum = 0;
     bool is_good = true;
     for (int i = 0; i < N; i++) {
         sum += i;
-        if (h_array[i] != sum) {is_good = False; break;}
+        if (h_array[i] != sum) {is_good = false; break;}
     }
-    
     if (is_good) {
         printf("✓ Test PASSED!\n");
     } else {
         printf("✗ Test FAILED!\n");
     }
 
-    cudaMemcpy(h_array, d_array, bytes, cudaMemcpyDeviceToHost);
     cudaFree(d_array);
     free(h_array);
     return 0;
